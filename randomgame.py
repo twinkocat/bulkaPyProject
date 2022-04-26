dbf0b13652259bf5f6c1eaaa022ba565e611f3c4
twinkocat <msbulahmaxim@gmail.com>
2022-04-26 09:47:08 +0300

    This my random game.
    Input two number and then any number in this range.

---
+++ randomgame.py
@@ -0,0 +1,35 @@
+import random
+
+def IntInput():
+    start=input()
+    end=input()
+    if start.isdigit() and end.isdigit():
+        start=int(start)
+        end=int(end)
+        ruletka(start, end)
+    else:
+        print('smth wrong')
+        return IntInput()
+
+def ruletka(start, end):
+    if start > end:
+        print('smth wrong')
+        IntInput()
+    else:
+        print(f'{start} to {end}')
+        a = int(input())
+        if a > 10:
+            print('smth wrong')
+            return ruletka(start, end)
+
+        b = random.randint(start, end)
+        while b != a:
+            print('You lose')
+            return ruletka(start, end)
+        else:
+            print('You won')
+
+print(IntInput())
+
+print(IntInput.__sizeof__())
+print(ruletka.__sizeof__())
