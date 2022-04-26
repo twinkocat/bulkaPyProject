600c5df9df054f66e51f0d38dc951fcbd5e9dfc2
twinkocat <msbulahmaxim@gmail.com>
2022-04-26 10:21:31 +0300

    Archimedean spiral.
    this my try to solve the taks.
    
    Original exercise http://judge.mipt.ru/mipt_cs_on_python3/labs/lab1.html#o2-s

---
+++ Архимедова спираль.py
@@ -0,0 +1,12 @@
+import turtle
+import math
+turtle.shape('turtle')
+
+rad=180/math.pi             # Высчитываем угол поворота спирали в радианах.
+p=(0.5/(2*math.pi))*rad     # 1 - шаг спирали
+l=0
+for spiral in range(1000):
+    l+=0.1
+    turtle.forward(l)
+    turtle.left(p)
+
